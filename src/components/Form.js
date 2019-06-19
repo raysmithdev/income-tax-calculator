5ef086136ff6f1dd90cf3840ee3a1f0e931335e7
raysmithdev <raysmithdev@gmail.com>
2019-06-19 11:26:15 +0100

    Added USC and other small bug fixes

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,14 +15,14 @@ export default function Form() {
       rate: 40,
       cutOff: 35301
     },
-    usc: [
+    uscLevels: [
       { level: 12012, rate: 0.5},
       { level: 7862, rate: 2},
       { level: 50170, rate: 4.5},
       { level: 50171, rate: 8}
     ],
     prsi: 4,
-    taxCredit: 1650
+    taxCredit: 3300
   }
 
   const [lowIncomeTax, setLowIncomeTax] = useState(0)
@@ -30,33 +30,57 @@ export default function Form() {
   const [prsi, setPrsi] = useState(0)
   const [totalTaxBill, setTotalTaxBill] = useState(0)
   const [showResults, setShowResults] = useState(false)
+  const [salary, setSalary] = useState('')
+  const [uscTax, setUscTax] = useState([])
+  const [totalUscTaxOwed, setTotalUscTaxOwed] = useState(0)
 
 
   const handleSubmit = input => {
-    const salary = input.nativeEvent.text
+    let salary = input.nativeEvent.text
 
     // Income Tax
     let salaryToBeTaxedAtHighIncomeRate = salary - rates.lowIncomeTax.cutOff
-    let lowIncomeTax = salary * rates.lowIncomeTax.rate / 100
+    let lowIncomeTax = rates.lowIncomeTax.cutOff * rates.lowIncomeTax.rate / 100
     let highIncomeTax = salaryToBeTaxedAtHighIncomeRate * rates.highIncomeTax.rate / 100
 
     setLowIncomeTax(lowIncomeTax)
     setHighIncomeTax(highIncomeTax)
 
-    // USC
-
-
     // PRSI
     let prsi = salary * rates.prsi / 100
     setPrsi(prsi)
 
-    // setTotalTaxBill
-    setTotalTaxBill(lowIncomeTax + highIncomeTax + prsi)
+    // USC
+
+    let counter = 0
+    let uscTax = []
+    let totalUscOwing = 0
+
+    // loop over levels of usc
+    rates.uscLevels.forEach(item => {
+
+      salary = salary - item.level
+
+      if(salary < 0) {
+        counter = counter + 1
+      }
+
+      if(counter <= 1) {
+        let tax = item.level * item.rate / 100
+        totalUscOwing += tax
+
+        return uscTax.push({rate: item.rate, level: item.level, tax})
+      }
+    })
+
+    setUscTax(uscTax)
+    setTotalUscTaxOwed(Math.round(totalUscOwing * 100) / 100)
+    // setTotalTaxBillOwing
+    setTotalTaxBill(lowIncomeTax + highIncomeTax + prsi + Math.round(totalUscOwing * 100) / 100)
 
     setShowResults(true)
   }
 
-  const [salary, setSalary] = useState('')
   return (
     <View style={styles.container}>
       <Input
@@ -70,7 +94,7 @@ export default function Form() {
           />
         }
       />
-      <Result data={{lowIncomeTax, highIncomeTax, prsi, totalTaxBill, showResults}}/>
+      <Result data={{lowIncomeTax, highIncomeTax, prsi, uscTax, totalUscTaxOwed, totalTaxBill, showResults}}/>
     </View>
   );
 }
--- src/components/Result.js
+++ src/components/Result.js
@@ -3,8 +3,6 @@ import { StyleSheet, Text, View } from 'react-native'
 import { ListItem, Divider } from 'react-native-elements'
 
 export default function Result(props) {
-  console.log(props.data);
-
   return (
     <View>
       {props.data.showResults
@@ -12,24 +10,37 @@ export default function Result(props) {
         <View style={styles.container}>
             <ListItem
               title={"Income Tax @ 20%"}
-              subtitle={`$ ${props.data.lowIncomeTax}`}
+              subtitle={`€ ${props.data.lowIncomeTax}`}
             />
             <ListItem
               title={"Income Tax @ 40%"}
-              subtitle={`$ ${props.data.highIncomeTax}`}
+              subtitle={`€ ${props.data.highIncomeTax}`}
             />
             <ListItem
               title={"Prsi @ 4%"}
-              subtitle={`$ ${props.data.prsi}`}
+              subtitle={`€ ${props.data.prsi}`}
+            />
+            <ListItem
+              title={"USC"}
+              subtitle={`€ ${props.data.totalUscTaxOwed}`}
             />
             <Divider style={{ backgroundColor: '#1A535C' }} />
             <ListItem
-              title={"Total Tax Owing"}
-              subtitle={`$ ${props.data.totalTaxBill}`}
+              title={"Gross Tax"}
+              subtitle={`€ ${props.data.totalTaxBill}`}
+            />
+            <ListItem
+              title={"Tax Credit"}
+              subtitle={"€3300"}
+            />
+            <Divider style={{ backgroundColor: '#1A535C' }} />
+            <ListItem
+              title={"Total Tax"}
+              subtitle={`€ ${Math.round((props.data.totalTaxBill - 3300) * 100) / 100}`}
             />
             <ListItem
               title={"Total Tax Owing Bi-Weekly"}
-              subtitle={`$ ${Math.round(props.data.totalTaxBill / 26 * 100) / 100}`}
+              subtitle={`€ ${Math.round((props.data.totalTaxBill - 3300) / 26 * 100) / 100}`}
             />
         </View> :
         null
